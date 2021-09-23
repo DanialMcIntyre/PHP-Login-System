6ed00c8e7442392f85530fab1337bd9e5429cc67
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2021-09-23 15:12:22 -0400

    Added a main index page that brings you to a login and register page

---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+<?php
+    if(!defined('__CONFIG__')) {
+        exit('You do not have a config file');
+    }
+?>
+
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.4/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.4/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.4/dist/js/uikit-icons.min.js"></script>
